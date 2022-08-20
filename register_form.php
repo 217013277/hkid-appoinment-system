1e0bf55bb6f7d7180a0ed37199dcae80259c3968
Yu Hong Yiu <97735756+217013277@users.noreply.github.com>
2022-08-20 18:44:03 +0800

    update register form

--- register_form.php
+++ register_form.php
@@ -5,8 +5,17 @@
 <body>
 <form action="register_result.php" method="post">
 <h1>Register</h1>
-User name: <input name="id" type="text" size="30" maxlength="100"><br><br>
-Password: <input name="pwd" type="text" size="30" maxlength="100"><br><br>
+User name: <input name="username" type="text" size="30" maxlength="100"><br><br>
+Password: <input name="password" type="text" size="30" maxlength="100"><br><br>
+English Name: <input name="engName" type="text" size="30" maxlength="100"><br><br>
+Chinese Name: <input name="ChiName" type="text" size="30" maxlength="100"><br><br>
+Gender: <input name="gender" type="text" size="30" maxlength="12"><br><br>
+Date of Birth: <input name="dob" type="date" size="30"><br><br>
+Address: <input name="address" type="text" size="100" maxlength="100"><br><br>
+Place of Birth: <input name="placeOfBirth" type="text" size="30" maxlength="100"><br><br>
+Occupation: <input name="occupation" type="text" size="30" maxlength="100"><br><br>
+HKID: <input name="hkid" type="text" size="30" maxlength="100"><br><br>
+
 <input name="submit" type="submit" value="submit">
 </form>
 </body>
