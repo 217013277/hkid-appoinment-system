7166490e2e38c391ad0c4f672cb40b0974afbd38
Yu Hong Yiu <97735756+217013277@users.noreply.github.com>
2022-08-22 16:44:35 +0800

    implement login function

--- login_result.php
+++ login_result.php
@@ -16,34 +16,27 @@ if ($conn->connect_error)
 
 /* Get user input which name is "id" and "pwd" 
 (assume the id and pwd has correct format) */
-$id = $_POST["id"]; 
-$pwd = $_POST["pwd"]; 
+$username = $_POST["username"]; 
+$password = $_POST["password"]; 
 
-// (2a) Write prepare statements to retrieve the columns salt and hash with the corresponding user
-
-/*______________________________(2a)______________________________*/
-    
-    $search_sql = $conn->prepare("SELECT * FROM userhash WHERE id=?");
-    $search_sql->bind_param("s", $id);
+// (2a) Write prepare statements to retrieve the columns salt and hash with the corresponding user  
+    $search_sql = $conn->prepare("SELECT username, salt, hash FROM userhash WHERE username=?");
+    $search_sql->bind_param("s", $username);
     $search_sql->execute();
     $search_sql->store_result();
 
 // If login name can be found in table "userhash"
-
 if($search_sql->num_rows > 0) 
 {       
     // (2b) Write a statement to generate a hash by using SHA512 algorithm and store it into variable $pwdhash (salt + password)
-    
-    /*______________________________(2b)______________________________*/
-    $search_sql->bind_result($i, $s, $h);
+    $search_sql->bind_result($username_db, $salt_db, $hash_db);
     $search_sql-> fetch();
     
-    
-    $pwdhash = hash("sha512", $s . $pwd);
+    $passwordHash = hash("sha512", $salt_db . $password);
 
     /* (2c)	Write a statement to compare the string content of $pwdhash and the hash value retrieved from database is equal (hint: use build in function strcmp) */
     
-    if(strcmp($h, $pwdhash) == 0)
+    if(strcmp($hash_db, $passwordHash) == 0)
     {
 	   echo "<h2>Authentication success!</h2>";
     }
