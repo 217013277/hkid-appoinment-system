c8dd1366fa22d89a95b06ad029936544a15e9364
yiuyu <217013277@stu.vtc.edu.hk>
2022-08-27 01:37:17 +0800

    implementing mail

---
+++ mail.php
@@ -0,0 +1,10 @@
+<?php
+// the message
+$msg = "First line of text\nSecond line of text";
+
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+// send email
+mail("217013277@stu.vtc.edu.hk","My subject",$msg);
+?>
