5fd3e2f5bc1569d1679ac97f297a077e01931e74
Yu Hong Yiu <97735756+217013277@users.noreply.github.com>
2022-09-02 14:35:35 +0800

    impelement appointment email

--- public/appointment/result.php
+++ public/appointment/result.php
@@ -70,15 +70,83 @@ $insert_appointment_sql = $conn->prepare("INSERT INTO `Appointments` (`User`,`Lo
 $insert_appointment_sql->bind_param("sss", $email, $location, $datetime);
 $insert_appointment_sql->execute();
 
-echo "<h2>Appointment success!</h2>";
-echo "<p>$location - $date $time</p>";
-
 // Close connection
 mysqli_close($conn);
 
+echo "<h2>Appointment success!</h2>";
+echo "<p>$location - $date $time</p>";
+
+mailTo($email,
+    "HKID Appointment detail",
+    "<p>$location - $date $time</p>",
+    "<p>Appointment detail has been sent to your email address</p>",
+    "sent fail"
+);
+
 ?>
 <a href='../userdetail'>Your user detail</a>
-<a href="../logout">logoutn</a>
+<a href="../logout">logout</a>
 </body>
 </html>
 
+<?php
+
+function mailTo($receiver, $subject, $content, $success, $fail){
+
+    include "../../../config.php";
+    
+    #
+    # Based on PHPMailer v6.0.5
+    # ref.: https://github.com/PHPMailer/PHPMailer
+    #
+    
+    require "../../../PHPMailer/src/PHPMailer.php";
+    require "../../../PHPMailer/src/Exception.php";
+    require "../../../PHPMailer/src/SMTP.php";
+    
+    $mail = new PHPMailer\PHPMailer\PHPMailer();
+    
+    try {
+        //Server settings
+        # $mail->SMTPDebug = 2; // Enable verbose debug output
+        $mail->isSMTP(); // Set mailer to use SMTP
+        $mail->Host = $emailhost; // Specify SMTP server
+        $mail->SMTPAuth = $emailsmtpauth; // Enable SMTP authentication
+        $mail->Username = $emailusername; // SMTP username
+        $mail->Password = $emailpassword; // SMTP password
+        $mail->SMTPSecure = $emailsmtpsecure; // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = $emailport; // TCP port to connect to
+    
+        $mail->SMTPOptions = array(
+            'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+            )
+        );
+    
+        //Recipients
+        $mail->setFrom($emailusername, 'Test Sender');
+        $mail->addAddress($receiver, 'Test receipt'); // Add a recipient
+        # $mail->addAddress('ellen@example.com'); // Name is optional
+        # $mail->addReplyTo('info@example.com', 'Information');
+        # $mail->addCC('cc@example.com');
+        # $mail->addBCC('bcc@example.com');
+    
+        //Attachments
+        # $mail->addAttachment('/var/tmp/file.tar.gz'); // Add attachments
+        # $mail->addAttachment('/tmp/image.jpg', 'new.jpg'); // Optional name
+    
+        //Content
+        $mail->isHTML(true); // Set email format to HTML
+        $mail->Subject = 'This is an appointment letter';
+        $mail->Body = $content;
+    
+        $mail->send();
+        echo $success;
+    } catch (Exception $e) {
+        echo $fail;
+        echo 'Mailer Error: ', $mail->ErrorInfo;
+        }
+    }
+?>
